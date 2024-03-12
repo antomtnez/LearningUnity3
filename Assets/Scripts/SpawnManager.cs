3e8eff1d22419bced665cbbbc8bd6d780136e8b2
Anto Martínez <antomartinezdev@gmail.com>
2024-03-12 13:38:03 +0100

    Unit 4 - I made a spawn enemy manager to spawn enemies in game

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    private float _spawnRange = 9f;
+
+    void Start(){
+        SpawnEnemy();
+    }
+
+    void SpawnEnemy(){
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+
+    Vector3 GenerateSpawnPosition(){
+        return new Vector3(Random.Range(-_spawnRange, _spawnRange), 0f, Random.Range(-_spawnRange, _spawnRange));
+    }
+}
