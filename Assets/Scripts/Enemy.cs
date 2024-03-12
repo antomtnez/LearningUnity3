a8c203b997f25adc1a39c8efcf68039e97e72c25
Anto Martínez <antomartinezdev@gmail.com>
2024-03-12 13:29:54 +0100

    Unit 4 - I made an enemy component to control the enemy movement against player

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody _enemyRb;
+    private GameObject _player;
+
+    void Start(){
+        _enemyRb = GetComponent<Rigidbody>();
+        _player = GameObject.Find("Player");
+    }
+
+    void Update(){
+        Vector3 lookDirection = (_player.transform.position - transform.position).normalized;
+        _enemyRb.AddForce(lookDirection * speed);
+    }
+}
